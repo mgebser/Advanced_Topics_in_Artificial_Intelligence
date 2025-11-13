2c36fc3cee529fe3b1814db3ac0a1ce518f0352e
mgebser <martin.gebser@aau.at>
2025-11-13 22:12:20 +0100

    actual solution files from session

--- Exercise_2/ricochet_robots/control.sol.py
+++ Exercise_2/ricochet_robots/control.sol.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python
 
-# import sys
+# COMMANDS TO RUN:
+# python control.py
+
 import signal
 import clingo
 from clingo import Number, Function
 
+# initialization of menu string, target and initial positions
 menu = "\n 0. EXIT\n"
 target = -1
+current = -1
 targets = []
-positions = []
+previous = []
+positions = [Function("pos", [Function("red", []), Number(1), Number(1), Number(0)]), Function("pos", [Function("blue", []), Number(1), Number(16), Number(0)]), Function("pos", [Function("green", []), Number(16), Number(1), Number(0)]), Function("pos", [Function("yellow", []), Number(16), Number(16), Number(0)])]
 
+# helper function to make search/optimization interruptible
 def signal_handler(signum, frame):
     print(" You pressed Ctrl+C!")
     handle.cancel()
 
+# helper function to get target position from user
 def get_target():
     global target
 
@@ -21,6 +28,7 @@ def get_target():
         target = int(input("TARGET: "))
         if target > len(targets): target = -1
 
+# print model(s) and collect initial positions for the next target (if any)
 def on_model(m):
     global positions
     global answer
@@ -33,17 +41,13 @@ def on_model(m):
         else: print(atom, end=" ")
     print("\nOptimization: " + str(m.cost[0]))
 
+# create a clingo instance to build the menu string
 ctl = clingo.Control()
 
 ctl.load("board.lp")
-ctl.load("ricochet_robots.sol.lp")
+ctl.load("ricochet_robots.lp")
 ctl.add("#show pos(R,X,Y,0) : pos(R,X,Y,moves).")
 ctl.ground([("base", [])])
-
-for atom in ctl.symbolic_atoms.by_signature("initial_pos", 3):
-    args = atom.symbol.arguments
-    args.append(Number(0))
-    positions.append(Function("pos", args))
 
 line = 1
 for atom in ctl.symbolic_atoms.by_signature("available_target", 4):
@@ -54,22 +58,44 @@ for atom in ctl.symbolic_atoms.by_signature("available_target", 4):
     line = line+1
 
 while target != 0:
+    # get the next target position from user
     print(menu)
     get_target()
 
     if target != 0:
+        if current > -1: ctl.assign_external(targets[current], False)
+
+        # prepare for the next target
         current = target-1
         target = -1
-        previous = positions
         answer = 1
 
-        for atom in previous: ctl.assign_external(atom, True)
+        for p in previous: ctl.assign_external(p, False)
+
+        # create a new clingo instance for the current target
+#        ctl = clingo.Control()
+
+        # load the logic program files
+#        ctl.load("board.lp")
+#        ctl.load("ricochet_robots.lp")
+
+        # inject initial positions and current target
+        # ctl.add("#show pos(R,X,Y,0) : pos(R,X,Y,moves)." + ".".join([str(p) for p in positions]) + "." + str(targets[current]) + ".")
+        # print("#show pos(R,X,Y,0) : pos(R,X,Y,moves)." + ".".join([str(p) for p in positions]) + "." + str(targets[current]) + ".")
+        print([str(p) for p in positions])
+        for p in positions:
+            ctl.assign_external(p, True)
+
+        print(str(targets[current]))
         ctl.assign_external(targets[current], True)
 
+        previous = positions
+
+        # ground instance and encoding for the current target
+#        ctl.ground([("base", [])])
+
+        # compute an optimal model/shortest plan unless interrupted by user
         with ctl.solve(on_model = on_model, async_ = True) as handle:
             signal.signal(signal.SIGINT, signal_handler)
             while not handle.wait(1): pass
             signal.signal(signal.SIGINT, signal.SIG_DFL)
-
-        for atom in previous: ctl.assign_external(atom, False)
-        ctl.assign_external(targets[current], False)
