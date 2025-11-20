98d340b384c2ae15e67d19ca5a845b46951e97da
mgebser <martin.gebser@aau.at>
2025-11-20 17:41:13 +0100

    current state

--- Project_2/control.py
+++ Project_2/control.py
@@ -25,33 +25,81 @@ def on_model(m):
     for atom in m.symbols(shown=True):
         args = atom.arguments
         if atom.name == "next_schedule":
-            print(atom.name + "(" + str(args[0]) + ")", end=" ")
-#            todo = True
-        elif atom.name == "next_at" or atom.name == "next_deliver" or atom.name == "next_priority":
-            n = args.pop(3)
+            n = args.pop(1).number
             if n == step:
-                state.append(Function(atom.name, args))
+                print(atom.name + "(" + str(args[0]) + ")", end=" ")
+                todo = True
+        elif atom.name == "next_at" or atom.name == "next_priority":
+            n = args.pop(3).number
+            if n == step:
+                state.append(Function(atom.name[5:], args))
+        elif atom.name == "next_deliver":
+            n = args.pop(3).number
+            if n == step:
+                state.append(Function("todo_" + atom.name[5:], args))
         elif atom.name == "next_call":
-            n = args.pop(3)
-            if args[2] == 0:
-                if n == step:
-                    state.append(Function(atom.name, args))
-            else:
-                print(atom, end=" ") # TODO
+            n = args.pop(3).number
+            if n == step:
+                if args[2].number == 0:
+                    todo_atom = Function("todo_" + atom.name[5:], args)
+                    state.append(todo_atom)
+                    print("TODO: " + str(todo_atom))
+                else:
+                    args.append(Number(n+1))
+                    event.append(Function(atom.name[5:], args))
+        elif atom.name == "next_call_deliver":
+            n = args.pop(4).number
+            if n == step:
+                args.append(Number(n+1))
+                event.append(Function(atom.name[5:], args))
         else: print(atom, end=" ")
     print("\nOptimization: " + str(m.cost[0]))
+#    print([str(a) for a in state])
+#    print([str(a) for a in event])
 
 while todo:
     step = step+1
     todo = False
     answer = 1
 
-    ctl = clingo.Control(arguments = ["--opt-strategy", "usc"])
+    print("===========================")
+    print("Call: " + str(step))
+
+    ctl = clingo.Control(arguments = ["--opt-strategy", "usc", "--warn", "none"])
     for arg in sys.argv[1:]: ctl.load(arg)
     ctl.load("next.lp")
 
-    if step == 0: ctl.add("at(E,F,0) :- init(E,F). todo_call(F,D,0) :- call(F,D). todo_deliver(E,F,0) :- deliver(E,F). priority(E,D,0) :- priority(E,D).")
+#    if step == 0: ctl.add("at(E,F,0) :- init(E,F). todo_call(F,D,0) :- call(F,D). todo_deliver(E,F,0) :- deliver(E,F). priority(E,D,0) :- priority(E,D).")
 
-    ctl.ground([("base", []), ("next", [Number(step)])])
+    ctl.ground([("instance", [])])
+
+    facts_name = "fact_" + str(step)
+    events_name = "events_" + str(step)
+    facts = ""
+    events = ""
+    if step == 0:
+        for atom in ctl.symbolic_atoms.by_signature("init", 2):
+            args = atom.symbol.arguments
+            facts += "at(" + str(args[0]) + "," + str(args[1]) + ",0)."
+        for atom in ctl.symbolic_atoms.by_signature("call", 2):
+            args = atom.symbol.arguments
+            facts += "todo_call(" + str(args[0]) + "," + str(args[1]) + ",0)."
+        for atom in ctl.symbolic_atoms.by_signature("deliver", 2):
+            args = atom.symbol.arguments
+            facts += "todo_deliver(" + str(args[0]) + "," + str(args[1]) + ",0)."
+        for atom in ctl.symbolic_atoms.by_signature("priority", 2):
+            args = atom.symbol.arguments
+            facts += "priority(" + str(args[0]) + "," + str(args[1]) + ",0)."
+    else:
+        facts = ".".join([str(a) for a in state]) + "."
+        events = ".".join([str(a) for a in event]) + "."
+        ctl.add(events_name, [], events)
+#        print(facts)
+#        print(events)
+    
+#    print(facts)
+    ctl.add(facts_name, [], facts)
+    ctl.ground([("base", []), ("next", [Number(step)]), (facts_name, []), (events_name, [])])
     ctl.solve(on_model = on_model)
+    if step == 3: sys.exit()
 
